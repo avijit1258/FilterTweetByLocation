fae741c5b65cd658d084a8210d3a2f3b6a658dcf
Avijit Bhattacharjee <avijit.bhattacharjee@usask.ca>
2020-11-19 22:44:23 +0000

    tweet summarization started.

---
+++ tweetSummarization.py
@@ -0,0 +1,8 @@
+from gensim.summarization.summarizer import summarize
+from gensim.summarization.textcleaner import clean_text_by_sentences as _clean_text_by_sentences
+from gensim.summarization.textcleaner import get_sentences
+
+
+# To Do
+# Use gensim summarization to summarize tweets
+
